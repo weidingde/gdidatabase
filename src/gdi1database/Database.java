9c3f35605c0237630b319f0bbdc2cf4ef466a105
ham <smithdodo@frotzy.local>
2014-07-08 19:07:43 +0200

    database

--- src/gdi1database/Database.java
+++ src/gdi1database/Database.java
@@ -1,5 +1,8 @@
 package gdi1database;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Set;
 
@@ -8,7 +11,7 @@ import gdi1database.items.DatabaseItem;
 public class Database<T extends DatabaseItem> implements
 		DatabaseInterface<DatabaseItem> {
 
-	HashMap<String, DatabaseItem> databank;
+	HashMap<String, DatabaseItem> databank;// the databank
 
 	public Database() {
 		databank = new HashMap<String, DatabaseItem>();
@@ -63,8 +66,44 @@ public class Database<T extends DatabaseItem> implements
 
 	@Override
 	public int getSize() {
-		// TODO Auto-generated method stub
-		return 0;
+		return databank.size();
 	}
 
+	@Override
+	public String toString() {
+		if (databank == null) {
+			// this will never happen, because databank can't be null, it will
+			// be
+			// initialized when this class is initialized
+			System.out.print("<<database is null>>");
+			return null;
+		} else if (databank.isEmpty()) {
+			System.out.print("<<database is empty>>");
+			return null;
+		} else {
+			// sort the data bank at first
+			this.tidyDatabank();
+			Collection<DatabaseItem> allData = databank.values();
+			StringBuilder report = new StringBuilder();
+			for (DatabaseItem e : allData) {
+				report.append(e.toString()).append("\n");
+			}
+			return report.toString();
+		}
+	}
+
+	// sort all data in databank by their keys
+	private void tidyDatabank() {
+		// get all keys, which is going to be sorted
+		String[] keys = (String[]) databank.keySet().toArray();
+		// use a new data bank to store sorted data
+		HashMap<String, DatabaseItem> tidyDB = new HashMap<String, DatabaseItem>();
+		Arrays.sort(keys);// sort keys
+		// construct the sorted data bank
+		for (String k : keys) {
+			tidyDB.put(k, databank.get(k));
+		}
+		// in the end replace the old data bank with new, sorted data bank
+		databank = tidyDB;
+	}
 }
