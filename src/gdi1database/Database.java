79757f26b514e26b2f4328723e2816b262585dc2
ham <smithdodo@frotzy.local>
2014-07-09 15:41:48 +0200

    database

--- src/gdi1database/Database.java
+++ src/gdi1database/Database.java
@@ -1,110 +1,72 @@
 package gdi1database;
 
-import java.util.Arrays;
+import gdi1database.items.DatabaseItem;
+
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Set;
 
-import gdi1database.items.DatabaseItem;
-
-public class Database<T extends DatabaseItem> implements
-		DatabaseInterface<DatabaseItem> {
-
-	HashMap<String, DatabaseItem> databank;// the databank
+public class Database<T extends DatabaseItem> implements DatabaseInterface<T> {
+	
+	private HashMap<String, T> cacheMap = new HashMap<String, T>();
 
 	public Database() {
-		databank = new HashMap<String, DatabaseItem>();
+		super();
+		this.cacheMap.clear();
 	}
 
-	@Override
-	public boolean addEntry(DatabaseItem e) {
-		// make sure that data bank doesn't have this data and its key has not
-		// been used
-		if (!databank.containsValue(e) && !databank.containsKey(e.getKey())) {
-			databank.put(e.getKey(), e);
-			return true;
-		} else
-			return false;
+	public boolean addEntry(T t) {
+		boolean flag = false;
+		if (!this.cacheMap.containsValue(t)
+				&& !this.cacheMap.containsKey(t.getKey())) {
+			this.cacheMap.put(t.getKey(), t);
+			flag = true;
+		}
+		return flag;
 	}
 
-	@Override
-	public boolean deleteEntry(DatabaseItem e) {
-		// make sure that exactly data e were deleted
-		if (databank.remove(e.getKey()) == e) {
-			return true;
-		} else
-			return false;
+	public boolean deleteEntry(T t) {
+		boolean flag = false;
+		if (this.cacheMap.remove(t.getKey()) != null) {
+			flag = true;
+		}
+		return flag;
 	}
 
-	@Override
 	public boolean dropDatabase() {
-		databank.clear();
-		if (databank.isEmpty()) {
-			return true;
-		} else
-			return false;
+		boolean flag = false;
+		try {
+			this.cacheMap.clear();
+			flag = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return flag;
 	}
 
-	@Override
-	public boolean entryExists(DatabaseItem e) {
-		if (databank.containsValue(e)) {
-			return true;
-		} else
-			return false;
+	public boolean entryExists(T t) {
+		return this.cacheMap.containsValue(t);
 	}
 
-	@Override
-	public DatabaseItem getEntry(String key) {
-		return databank.get(key);
+	public T getEntry(String key) {
+		return this.cacheMap.get(key);
 	}
 
-	@Override
 	public Set<String> getKeys() {
-		return databank.keySet();
+		return this.cacheMap.keySet();
 	}
 
-	@Override
 	public int getSize() {
-		return databank.size();
+		return this.cacheMap.size();
 	}
 
-	@Override
-	public String toString() {
-		if (databank == null) {
-			// this will never happen, because databank can't be null, it will
-			// be
-			// initialized when this class is initialized
-			System.out.print("<<database is null>>");
-			return null;
-		} else if (databank.isEmpty()) {
-			System.out.print("<<database is empty>>");
-			return null;
-		} else {
-			// sort the data bank at first
-			this.tidyDatabank();
-			Collection<DatabaseItem> allData = databank.values();
-			StringBuilder report = new StringBuilder();
-			for (DatabaseItem e : allData) {
-				report.append(e.toString()).append("\n");
-			}
-			return report.toString();
-		}
-	}
-
-	/**
-	 * sort the data bank by its key
+	/***
+	 * Add by myself, it used to query data easily!
+	 * 
+	 * @return
 	 */
-	private void tidyDatabank() {
-		// get all keys, which is going to be sorted
-		String[] keys = (String[]) databank.keySet().toArray();
-		// use a new data bank to store sorted data
-		HashMap<String, DatabaseItem> tidyDB = new HashMap<String, DatabaseItem>();
-		Arrays.sort(keys);// sort keys
-		// construct the sorted data bank
-		for (String k : keys) {
-			tidyDB.put(k, databank.get(k));
-		}
-		// in the end replace the old data bank with new, sorted data bank
-		databank = tidyDB;
+	public Collection<T> getAll() {
+		return this.cacheMap.values();
 	}
+
 }
