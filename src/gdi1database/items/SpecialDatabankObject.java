4d082867e0a7f4d94ffedd9072cc666acb915b6d
matchlesstinkle <dingwei198712@gmail.com>
2014-07-10 23:31:20 +0200

    Update SpecialDatabankObject.java

--- src/gdi1database/items/SpecialDatabankObject.java
+++ src/gdi1database/items/SpecialDatabankObject.java
@@ -1,3 +1,8 @@
+/**
+ * creat the Zwischenklasse to avoid the redundant.
+ * @author Hengwen Zhang, Wei Ding, Julius Friedrich Muth, Cong Chen
+ * @version 2.0
+ */
 package gdi1database.items;
 
 /**
